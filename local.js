9229de28f335d21ca4faf2a453ec1136e1c4157f
axl <axl.van.nguyen@gmail.com>
2025-07-13 11:31:11 -0400

    added files

---
+++ local.js
@@ -0,0 +1,23 @@
+function storeName(){
+    var username = document.getElementById("username").value;
+    if(username === "null" || username ===""){
+        return false;
+    }
+    localStorage.setItem("user", username);
+    document.getElementById("username").value = "";
+    document.getElementById("leg").innerHTML ="\"" + localStorage.getItem("user") + "\" is Stored";
+}
+function recallName(){
+    if(localStorage.getItem("user") === null){
+        document.getElementById("username").value = "";
+        document.getElementById("leg").innerHTML ="Stored name is\"" + localStorage.getItem("user") + "\"";
+    }
+}
+function removeName(){
+    if(localStorage.getItem("user") === null){
+        return false;
+    }
+    document.getElementById("leg").innerHTML = "\"" + localStorage.getItem("user") + "\" is removed";
+    localStorage.removeItem("user");
+    document.getElementById("username").value = "";
+}
